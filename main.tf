893c5cfe3af3b5d1524b884e60706caf11accbb3
David Negstad <davidn@pk4media.com>
2016-01-08 16:28:08 -0800

    Separate consul and consul client setup scripts

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 output "ubuntu_consul_tls_setup" { value = "${path.module}/ubuntu/consul_tls.sh"}
 output "ubuntu_consul_setup"     { value = "${path.module}/ubuntu/consul.sh" }
+output "ubuntu_consul_client_setup" { value = "${path.module}/ubuntu/consul_client.sh" }
 output "ubuntu_vault_tls_setup"  { value = "${path.module}/ubuntu/vault_tls.sh" }
 output "ubuntu_vault_setup"      { value = "${path.module}/ubuntu/vault.sh" }
