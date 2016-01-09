ce5a204c5b4ede30fb67ae9ea787262d76110df5
David Negstad <davidn@pk4media.com>
2016-01-08 18:28:40 -0800

    Update consul tls script

--- ubuntu/consul_tls.sh
+++ ubuntu/consul_tls.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sudo mkdir -p /opt/consul/tls
-
 sudo cat <<EOF >/opt/consul/tls/ca.pem
 ${ca}
 EOF
+sudo chown root:consul /opt/consul/tls/ca.pem
+sudo chmod 640 /opt/consul/tls/ca.pem
 
 sudo cat <<EOF >/opt/consul/tls/cert.pem
 ${cert}
 EOF
+sudo chown root:consul /opt/consul/tls/cert.pem
+sudo chmod 640 /opt/consul/tls/cert.pem
 
 sudo cat <<EOF >/opt/consul/tls/key.pem
 ${key}
 EOF
+sudo chown root:consul /opt/consul/tls/key.pem
+sudo chmod 640 /opt/consul/tls/key.pem
