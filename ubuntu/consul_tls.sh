70dc371964eafca889b665353afa8ea2f90873ed
David Negstad <davidn@pk4media.com>
2016-01-10 13:44:30 -0800

    Don't try and chown consul keys

--- ubuntu/consul_tls.sh
+++ ubuntu/consul_tls.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 echo "${ca}" | sudo tee /opt/consul/tls/ca.pem
-sudo chown root:consul /opt/consul/tls/ca.pem
 sudo chmod 640 /opt/consul/tls/ca.pem
 
 echo "${cert}" | sudo tee /opt/consul/tls/cert.pem
-sudo chown root:consul /opt/consul/tls/cert.pem
 sudo chmod 640 /opt/consul/tls/cert.pem
 
 echo "${key}" | sudo tee /opt/consul/tls/key.pem
-sudo chown root:consul /opt/consul/tls/key.pem
 sudo chmod 640 /opt/consul/tls/key.pem
