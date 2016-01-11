b6d274179289d47e5dd7f7134b15fd071cf54f61
David Negstad <davidn@pk4media.com>
2016-01-10 18:42:18 -0800

    Update vault scripts

--- ubuntu/vault.sh
+++ ubuntu/vault.sh
@@ -2,9 +2,9 @@
 
 METADATA_INSTANCE_ID=`curl http://169.254.169.254/2014-02-25/meta-data/instance-id`
 
-sed -i -- "s/{{ address }}/${private_ip}/g" /etc/vault.d/vault_config.hcl
+sudo sed -i -- "s/{{ address }}/${private_ip}/g" /etc/vault.d/vault_config.hcl
 
-service vault restart
+sudo service vault restart
 
 echo "Vault config updated"
 
