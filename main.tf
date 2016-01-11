7d98201e57894db86598eb390bf073841424b79a
David Negstad <davidn@pk4media.com>
2016-01-10 17:23:40 -0800

    Update scripts

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-output "ubuntu_consul_tls_setup" { value = "${path.module}/ubuntu/consul_tls.sh"}
-output "ubuntu_consul_setup"     { value = "${path.module}/ubuntu/consul.sh" }
-output "ubuntu_consul_client_setup" { value = "${path.module}/ubuntu/consul_client.sh" }
+output "ubuntu_consul_tls_setup" { value = "${path.module}/ubuntu/consul/tls.sh"}
+output "ubuntu_consul_setup"     { value = "${path.module}/ubuntu/consul/setup.sh" }
+output "ubuntu_consul_client_setup" { value = "${path.module}/ubuntu/consul/client_setup.sh" }
 output "ubuntu_vault_tls_setup"  { value = "${path.module}/ubuntu/vault_tls.sh" }
 output "ubuntu_vault_setup"      { value = "${path.module}/ubuntu/vault.sh" }
