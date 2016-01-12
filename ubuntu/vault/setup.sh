6f90bcaa184324ea9c47f29185848567f50ff4c0
David Negstad <davidn@pk4media.com>
2016-01-12 00:41:30 -0800

    Wrong vault install folder

--- ubuntu/vault/setup.sh
+++ ubuntu/vault/setup.sh
@@ -2,7 +2,7 @@
 
 METADATA_INSTANCE_ID=`curl http://169.254.169.254/2014-02-25/meta-data/instance-id`
 
-sudo sed -i -- "s/{{ address }}/${private_ip}/g" /etc/vault.d/vault_config.hcl
+sudo sed -i -- "s/{{ address }}/${private_ip}/g" /opt/vault/vault_config.hcl
 
 sudo service vault restart
 
